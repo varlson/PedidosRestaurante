df318a579cb949365d6349dfcb0d36e6cf4f064c
Hugo Ziviani <hugo_ziviani@hotmail.com>
2021-04-02 08:57:57 -0300

    starting the code

--- restaurante/src/App.js
+++ restaurante/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import { Router, Link } from '@reach/router';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    
+    constructor(param){ 
+        super(param)
+        
+        // this.items = [
+        //     { to: '/', label:'Home'}
+        // ];
+    }
+    render(){
+        return(
+            <div className='app'>
+                {/* <Router> */}
+                  <p>Aoooba</p>
+                {/* </Router> */}
+          </div>  
+
+        );
+    }
 }
 
 export default App;
